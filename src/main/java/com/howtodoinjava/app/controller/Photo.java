e7ca024eb77fb0234c7b990d02556d4192c89ae9
mac <Mamaiev>
2020-05-05 15:44:58 +0300

    Added Photo class

---
+++ src/main/java/com/howtodoinjava/app/controller/Photo.java
@@ -0,0 +1,40 @@
+package com.howtodoinjava.app.controller;
+
+public class Photo {
+
+    private String name;
+    private String l; // TODO: refactoring name of variable
+    private String a;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getL() {
+        return l;
+    }
+
+    public void setL(String l) {
+        if (l != null) {
+            this.l = l.replace(",", ".");
+        } else {
+            this.l = null;
+        }
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        if (a != null) {
+            this.a = a.replace(",", ".");
+        } else {
+            this.a = null;
+        }
+    }
+}
